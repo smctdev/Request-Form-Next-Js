be73f2853b2c050f2399f7436dd4dd760cce532a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-05-29 17:29:30 +0800

    new featured updated

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -2,7 +2,7 @@ export default function Textarea({ ...props }: any) {
   return (
     <textarea
       {...props}
-      className="h-24 textarea textarea-bordered bg-gray-50 focus:outline-none focus:ring-1 focus:border-primary"
+      className="h-24 textarea textarea-bordered bg-gray-50 focus:outline-none focus:ring-1 focus:border-primary w-full"
     ></textarea>
   );
 }
---
+++ src/constants/form-data.ts
@@ -0,0 +1,9 @@
+export const feedbackData = {
+  name: "",
+  email: "",
+  phone: "",
+  department: "",
+  opinion: "",
+  other_opinion: "",
+  message: "",
+};
