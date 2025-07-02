6cbecdaa271122fdf50efb85370a779122cca5e9
smctdev <168695959+smctdev@users.noreply.github.com>
2025-07-02 11:09:38 +0800

    updated

--- src/app/(views)/approver/_components/modals/ApproverCashAdvance.tsx
+++ src/app/(views)/approver/_components/modals/ApproverCashAdvance.tsx
@@ -639,7 +639,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
           <div className="flex flex-wrap items-center gap-2">
             <h1 className="text-sm font-medium">Liquidation Date:</h1>
             <span className="pl-1 font-bold bg-white rounded-md">
-              {formatDate(record?.form_data[0]?.liquidationDate)}
+              {record?.form_data[0]?.liquidationDate}
             </span>
           </div>
           <div className="flex w-full mt-2 space-x-2">
