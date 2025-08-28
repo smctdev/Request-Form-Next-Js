ab17f4e982eaaf5b8f59dda5a3c3b08f8b356011
smctdev <168695959+smctdev@users.noreply.github.com>
2025-08-28 11:36:11 +0800

    added important true

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
