a91ebc0492718bff8735b14734813005a5eb5099
smctdev <168695959+smctdev@users.noreply.github.com>
2025-08-28 14:33:50 +0800

    revert back to original tailwindcss

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,27 +2,15 @@ import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  important: true,
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        blue: colors.blue,
-        red: colors.red,
-        gray: colors.gray,
-        black: colors.black,
-        white: colors.white,
-      },
-     },
+    extend: { /* your customizations */ },
   },
   plugins: [
     daisyui,
   ],
-  daisyui: {
-    themes: ["light", "dark"], // keep DaisyUI themes available
-  },
 };
