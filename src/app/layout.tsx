a35b9c793c8d3cc77ba6faf11c950fff9045ac59
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-09-01 10:08:27 +0800

    t

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./app.css";
 import { AuthProvider } from "../context/AuthContext";
 import BaseContent from "@/components/layouts/BaseContent";
 import { NotificationProvider } from "@/context/NotificationContext";
