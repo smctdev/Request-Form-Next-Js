c0c54a8953f624f35a9818cc3e1224aca83f7e1b
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-06-16 17:46:57 +0800

    added storage and remove ${process.env.NEXT_PUBLIC_API_STORAGE_URL}/

--- src/app/(views)/approver/_components/modals/ApproverStock.tsx
+++ src/app/(views)/approver/_components/modals/ApproverStock.tsx
@@ -138,7 +138,7 @@ const ApproversStock: React.FC<Props> = ({
     (user) => user.status === "Disapproved",
   );
   const [isImgModalOpen, setIsImgModalOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState(null);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
   const [zoom, setZoom] = useState(1);
   const [dragging, setDragging] = useState(false);
   const [positionImg, setPositionImg] = useState({ x: 0, y: 0 });
@@ -374,7 +374,7 @@ const ApproversStock: React.FC<Props> = ({
   };
 
   const handleViewImage = (imageUrl: any) => {
-    setCurrentImage(imageUrl);
+    setCurrentImage(Storage(imageUrl));
     setIsImgModalOpen(true);
   };
 
@@ -763,11 +763,7 @@ const ApproversStock: React.FC<Props> = ({
                         </a>
 
                         <button
-                          onClick={() =>
-                            handleViewImage(
-                              `${process.env.NEXT_PUBLIC_API_STORAGE_URL}/${attachmentItem}`,
-                            )
-                          }
+                          onClick={() => handleViewImage(`${attachmentItem}`)}
                           className="focus:outline-none tooltip tooltip-info tooltip-top"
                           data-tip="View"
                         >
