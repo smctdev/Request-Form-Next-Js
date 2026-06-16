e495e2201dac8c2558d3cb522d3bde4cc6f0eaca
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-06-16 17:07:14 +0800

    updated type errors

--- src/app/(views)/approver/_components/modals/ApproverCashAdvance.tsx
+++ src/app/(views)/approver/_components/modals/ApproverCashAdvance.tsx
@@ -48,7 +48,10 @@ type Record = {
   form_data: FormData[];
   supplier?: string;
   address?: string;
-  branch: string;
+  branch: {
+    name: string;
+    branch: string;
+  };
   date: string;
   user_id: number;
   attachment: string;
@@ -123,7 +126,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
   const [isEditing, setIsEditing] = useState(false);
   const [editedDate, setEditedDate] = useState("");
   const [editedApprovers, setEditedApprovers] = useState<number>(
-    record.approvers_id
+    record.approvers_id,
   );
   const [attachment, setAttachment] = useState<any>([]);
   const [file, setFile] = useState<File[]>([]);
@@ -142,16 +145,16 @@ const ApproverCashAdvance: React.FC<Props> = ({
   const [printWindow, setPrintWindow] = useState<Window | null>(null);
   const [attachmentUrl, setAttachmentUrl] = useState<string[]>([]);
   const [modalStatus, setModalStatus] = useState<"approved" | "disapproved">(
-    "approved"
+    "approved",
   );
   let logo;
   const [branchList, setBranchList] = useState<any[]>([]);
   const [branchMap, setBranchMap] = useState<Map<number, string>>(new Map());
   const hasDisapprovedInNotedBy = notedBy.some(
-    (user) => user.status === "Disapproved"
+    (user) => user.status === "Disapproved",
   );
   const hasDisapprovedInApprovedBy = approvedBy.some(
-    (user) => user.status === "Disapproved"
+    (user) => user.status === "Disapproved",
   );
   const [isImgModalOpen, setIsImgModalOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(null);
@@ -186,7 +189,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
           branches.map((branch: { id: number; branch_code: string }) => [
             branch.id,
             branch.branch_code,
-          ])
+          ]),
         );
 
         setBranchList(branches);
@@ -219,8 +222,8 @@ const ApproverCashAdvance: React.FC<Props> = ({
           (filePath: string) =>
             `${process.env.NEXT_PUBLIC_API_STORAGE_URL}/${filePath.replace(
               /\\/g,
-              "/"
-            )}`
+              "/",
+            )}`,
         );
         setAttachmentUrl(fileUrls);
       } else {
@@ -244,13 +247,13 @@ const ApproverCashAdvance: React.FC<Props> = ({
         } else {
           console.warn(
             "Parsed approved attachment is not an array or is empty:",
-            parsedApprovedAttachment
+            parsedApprovedAttachment,
           );
         }
       } else {
         console.warn(
           "Approved attachment is not an array or is empty:",
-          record.approved_attachment
+          record.approved_attachment,
         );
       }
     } catch (error) {
@@ -284,7 +287,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
 
       const response = await api.post(
         `/request-forms/${record.id}/process`,
-        requestData
+        requestData,
       );
 
       setLoading(false);
@@ -339,7 +342,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
 
       const response = await api.post(
         `/request-forms/${record.id}/process`,
-        requestData
+        requestData,
       );
 
       setApprovedLoading(false);
@@ -379,7 +382,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
     total += parseFloat(newTotalContingency);
     total += newData.reduce(
       (totalPerDiem, item) => totalPerDiem + Number(item.perDiem),
-      0
+      0,
     );
     return parseFloat(total.toString()).toFixed(2);
   };
@@ -408,7 +411,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
   const handleItemChange = (
     index: number,
     field: keyof Item,
-    value: string
+    value: string,
   ) => {
     const newDataCopy = [...newData];
     newDataCopy[index] = { ...newDataCopy[index], [field]: value };
@@ -560,7 +563,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
 
   const handleRemoveImage = (imageName: string) => {
     setFile((prevImages) =>
-      prevImages.filter((image) => image.name !== imageName)
+      prevImages.filter((image) => image.name !== imageName),
     );
   };
 
@@ -598,7 +601,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
             Application for Cash Advance
           </h1>
           <div className="flex flex-col justify-center ">
-            <p className="underline ">{record?.branch}</p>
+            <p className="underline ">{record?.branch?.name}</p>
             <p className="text-center">Branch</p>
           </div>
         </div>
@@ -620,10 +623,10 @@ const ApproverCashAdvance: React.FC<Props> = ({
                   record.status.trim() === "Pending"
                     ? "bg-yellow-400"
                     : record.status.trim() === "Approved"
-                    ? "bg-green-400"
-                    : record.status.trim() === "Disapproved"
-                    ? "bg-pink-400"
-                    : "bg-pink-400"
+                      ? "bg-green-400"
+                      : record.status.trim() === "Disapproved"
+                        ? "bg-pink-400"
+                        : "bg-pink-400"
                 } rounded-lg  py-1 w-1/3 font-medium text-[14px] text-center ml-2 text-white`}
               >
                 {" "}
@@ -674,7 +677,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "cashDate",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -687,7 +690,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "day",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -700,7 +703,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "itinerary",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -713,7 +716,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "activity",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -726,7 +729,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "hotel",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -739,7 +742,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "rate",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -752,7 +755,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "amount",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -765,7 +768,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "perDiem",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -778,7 +781,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                     handleItemChange(
                                       index,
                                       "remarks",
-                                      e.target.value
+                                      e.target.value,
                                     )
                                   }
                                 />
@@ -812,7 +815,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                   {item.remarks}
                                 </td>
                               </tr>
-                            )
+                            ),
                           )}
                     </tbody>
                   </table>
@@ -848,7 +851,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                         />
                       ) : (
                         formattedAmount(
-                          editableRecord.form_data[0].totalBoatFare ?? 0
+                          editableRecord.form_data[0].totalBoatFare ?? 0,
                         )
                       )}
                     </td>
@@ -868,7 +871,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                         />
                       ) : (
                         formattedAmount(
-                          editableRecord.form_data[0].totalHotel ?? 0
+                          editableRecord.form_data[0].totalHotel ?? 0,
                         )
                       )}
                     </td>
@@ -883,8 +886,8 @@ const ApproverCashAdvance: React.FC<Props> = ({
                         newData.reduce(
                           (totalPerDiem, item) =>
                             totalPerDiem + Number(item.perDiem),
-                          0
-                        )
+                          0,
+                        ),
                       )}
                     </td>
                   </tr>
@@ -903,7 +906,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                         />
                       ) : (
                         formattedAmount(
-                          editableRecord.form_data[0].totalFare ?? 0
+                          editableRecord.form_data[0].totalFare ?? 0,
                         )
                       )}
                     </td>
@@ -925,7 +928,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                         />
                       ) : (
                         formattedAmount(
-                          editableRecord.form_data[0].totalContingency ?? 0
+                          editableRecord.form_data[0].totalContingency ?? 0,
                         )
                       )}
                     </td>
@@ -940,7 +943,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                       {formattedAmount(
                         isEditing
                           ? calculateGrandTotal()
-                          : editableRecord.form_data[0].grand_total
+                          : editableRecord.form_data[0].grand_total,
                       )}
                     </td>
                   </tr>
@@ -994,10 +997,10 @@ const ApproverCashAdvance: React.FC<Props> = ({
                               user.status === "Approved"
                                 ? "text-green-400"
                                 : user.status === "Pending"
-                                ? "text-yellow-400"
-                                : user.status === "Rejected"
-                                ? "text-red"
-                                : ""
+                                  ? "text-yellow-400"
+                                  : user.status === "Rejected"
+                                    ? "text-red"
+                                    : ""
                             }`}
                           >
                             {user.status}
@@ -1060,8 +1063,8 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                   user.status === "Approved"
                                     ? "text-green-400"
                                     : user.status === "Pending"
-                                    ? "text-yellow-400"
-                                    : ""
+                                      ? "text-yellow-400"
+                                      : ""
                                 }`}
                               >
                                 {user.status}
@@ -1125,8 +1128,8 @@ const ApproverCashAdvance: React.FC<Props> = ({
                                 user.status === "Approved"
                                   ? "text-green-400"
                                   : user.status === "Pending"
-                                  ? "text-yellow-400"
-                                  : ""
+                                    ? "text-yellow-400"
+                                    : ""
                               }`}
                             >
                               {user.status}
@@ -1392,7 +1395,7 @@ const ApproverCashAdvance: React.FC<Props> = ({
                         <button
                           onClick={() =>
                             handleViewImage(
-                              `${process.env.NEXT_PUBLIC_API_STORAGE_URL}/${attachmentItem}`
+                              `${process.env.NEXT_PUBLIC_API_STORAGE_URL}/${attachmentItem}`,
                             )
                           }
                           className="focus:outline-none tooltip tooltip-info tooltip-top"
