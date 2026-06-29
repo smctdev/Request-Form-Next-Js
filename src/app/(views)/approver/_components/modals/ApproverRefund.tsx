432f84bae28999c5f3f440ab0f661fbdc56f6c07
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-06-29 08:28:13 +0800

    approved attachments

--- src/app/(views)/approver/_components/modals/ApproverRefund.tsx
+++ src/app/(views)/approver/_components/modals/ApproverRefund.tsx
@@ -1073,7 +1073,7 @@ const ApproverRefund: React.FC<Props> = ({
               )}
             </ul>
           </div>
-          <div className="w-full max-w-full ">
+          {/* <div className="w-full max-w-full ">
             <p className="font-semibold">Approved Attachment:</p>
 
             {record.approved_attachment.length === 0 &&
@@ -1156,7 +1156,7 @@ const ApproverRefund: React.FC<Props> = ({
                 Remove All
               </button>
             </div>
-          )}
+          )} */}
           <div className="max-w-[500px] overflow-x-auto pb-3 ">
             <div className="flex gap-1">
               {file.map((fileItem) => (
