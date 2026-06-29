c3680d48ea265dc23612d12cc40ffdab14e08639
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2026-06-29 08:28:02 +0800

    approved attachments

--- src/app/(views)/approver/_components/modals/ApproverChecklssuance.tsx
+++ src/app/(views)/approver/_components/modals/ApproverChecklssuance.tsx
@@ -769,7 +769,7 @@ const ApproverCheckIssuance: React.FC<Props> = ({
               )}
             </ul>
           </div>
-          <div className="w-full max-w-full ">
+          {/* <div className="w-full max-w-full ">
             <p className="font-semibold">Approved Attachment:</p>
 
             {record.approved_attachment.length === 0 &&
@@ -848,7 +848,7 @@ const ApproverCheckIssuance: React.FC<Props> = ({
                 Remove All
               </button>
             </div>
-          )}
+          )} */}
           <div className="max-w-[500px] overflow-x-auto pb-3 ">
             <div className="flex gap-1">
               {file.map((fileItem) => (
